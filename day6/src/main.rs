ce63809b08a7a11749cb23d104209f9c406eaf58
Chris Merck <cmerck@dagobah>
2023-01-01 12:28:44 -0500

    Day 6: Part 2, corrected off-by-1 error

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,12 +1,14 @@
 
 // find start-of-packet marker
 // which is the 1-based index of the first character in the string
-// for which the past 4 characters (inclusive) are all different
-fn find_marker(s: &str) -> usize {
+// for which the past n characters (inclusive) are all different
+// add a parameter for the number of characters to check
+fn find_marker(s: &str, n: usize) -> usize {
     let mut marker = 0;
-    for i in 4..s.len() {
-        // get the 4 characters before the current character
-        let chars = &s[i - 4..i + 1];
+    for i in n..s.len() {
+        // get the n characters before the current character
+        let chars = &s[i - n..i];
+        //println!("   {} -> {}", i, chars);
         // check if all characters are different
         if chars.chars().all(|c| chars.chars().filter(|&x| x == c).count() == 1) {
             marker = i;
@@ -28,16 +30,14 @@ fn main() {
     ];
 
     for (s, expected) in &examples {
-        let marker = find_marker(s);
-        println!("{} -> {}", s, marker);
-        if marker != *expected {
-            println!("  Expected {} but got {}", expected, marker);
-        }
+        let marker_4 = find_marker(s, 4);
+        let marker_14 = find_marker(s, 14);
+        println!("{} -> {} {}", s, marker_4, marker_14);
     }
 
     // now run the real input reading from file "input"
     let input = std::fs::read_to_string("input").unwrap();
-    let marker = find_marker(&input);
-    println!("{} -> {}", input, marker);
-
+    let marker_4 = find_marker(&input, 4);
+    let marker_14 = find_marker(&input, 14);
+    println!("{} -> {} {}", input, marker_4, marker_14);
 }
